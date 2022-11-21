3ddd089108f13ed32dd967d39b2ce6d6c2426d72
JaimeBerd <105523676+JaimeBerd@users.noreply.github.com>
2022-11-21 14:52:08 -0800

    adding data and update analysis

--- source/analysis.R
+++ source/analysis.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(dplyr)
 
 # The functions might be useful for A4
 source("../source/a4-helpers.R")
@@ -20,8 +21,31 @@ test_query2 <- function(num=6) {
 
 ## Section 2  ---- 
 #----------------------------------------------------------------------------#
-# Your functions and variables might go here ... <todo: update comment>
+# Getting data from github and getting 3 critical values.
 #----------------------------------------------------------------------------#
+jail_data <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
+View(jail_data)
+
+## In the following calculations, "incarceration = prison"
+# Value 1: What percentage of incarcerated people were black in 2018?
+black_percentage_2018 <- jail_data %>%
+  select(year, state, total_prison_pop, black_prison_pop) %>% 
+  filter(year == "2018") %>% 
+  group_by(state)
+  
+View(black_percentage_2018)
+# Value 2: What percentage of incarcerated individuals were incarcerated in the year 2018?
+
+# Value 3: In which year did total incarceration numbers (for the entire US) increase the most?
+most_increase_year <- jail_data %>% 
+  select(year, total_prison_pop) %>% 
+  drop_na() %>% 
+  group_by(year) %>% 
+  summarize(total_prison_pop = sum(total_prison_pop)) %>% 
+  mutate(difference = total_prison_pop - lag(total_prison_pop)) %>% 
+  drop_na() %>% 
+  filter(difference == max(difference)) %>% 
+  pull(year)
 
 ## Section 3  ---- 
 #----------------------------------------------------------------------------#
